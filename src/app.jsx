362354b0d9231aa7d7082c127bb9417566f0a8aa
raynyonathan <raynyonathanentertaiment@gmail.com>
2026-04-27 20:50:42 +0800

    Refactor app.jsx for tournament bracket display

--- src/app.jsx
+++ src/app.jsx
@@ -29,7 +29,7 @@ const BRACKET_DATA = {
   // sang juara
   finalLabel: "JUARA",
   juaraLabel: "CHAMPION",
-  namaJuara: "HIDUP JOKOWI!!!" 
+  namaJuara: "HIDUP JAWA!!!" 
 };
   //jangan pernah sentuh kode dibawah!!!!!!!!🤬😡😡😡
 const App = () => {
